7c339158ef8d4722f24a4a4330941f8355f679a0
KyoheiKato <s1411360@coins.tsukuba.ac.jp>
2015-01-21 14:13:34 +0900

    Modify main function

--- practice-1/ranGen.c
+++ practice-1/ranGen.c
@@ -2,27 +2,24 @@
 #include<stdlib.h>
 
 int main(){
-		int pointNum;
-		int seed;
-		double x,y;
-		printf("seed :");
-		scanf("%d", &seed);
-		printf("point_num:");
-		scanf("%d", &pointNum);
-		srand(seed);
-		FILE *outputFile;
-		char *fileName;
-		fileName = "points.csv";
-		outputFile = fopen(fileName, "w");
-		for(int i = 0; i < pointNum;){
-				x = (double) (3.0 * rand() / (RAND_MAX + 1.0)) - 1;
-				y = (double) (3.0 * rand() / (RAND_MAX + 1.0)) - 1;
-				if ((x*x + y*y) <= 1){
-						fprintf(outputFile, "%f,%f\n", x, y);
-						i++;
-				} 
-		}
-		fclose(outputFile);	
+	const int point = 500;
+	const int seed = 149;
+	double x,y;
+	FILE *outputFile;
+	char *fileName = "points.csv";
+
+	srand(seed);
+	outputFile = fopen(fileName, "w");
 	
-		return 0;
+	for(int i = 0; i < point;){
+		x = (double) (2.0 * rand() / (RAND_MAX + 1.0)) - 1;
+		y = (double) (2.0 * rand() / (RAND_MAX + 1.0)) - 1;
+		if ((x*x + y*y) <= 1){
+			fprintf(outputFile, "%f,%f\n", x, y);
+			i++;
+		} 
+	}
+	fclose(outputFile);	
+	
+	return 0;
 }
